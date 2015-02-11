60e9c180e3fc2255a23924d314e75c3eb437b8a3
Bryan W. Weber <bryan@bryanwweber.com>
2015-02-11 11:42:47 -0500

    Simplify recompile-figures.py

--- figures/recompile-figures.py
+++ figures/recompile-figures.py
@@ -25,7 +25,7 @@ class cd:
 with open('figures-to-redo.txt','r') as inputfile:
     lines = inputfile.readlines()
 
-tex_command = ['latexmk', '-pdf', '-pdflatex=lualatex', '-silent', '']
+tex_command = ['latexmk', '-lualatex', '-silent']
 
 lines = [line.strip() for line in lines]
 for line in lines:
@@ -34,7 +34,6 @@ for line in lines:
     l = line.split('/')[1]
     with cd(line):
         print(line,os.path.isfile(l + '.tex'))
-        tex_command[-1:] = [l]
         call(tex_command)
         shutil.copyfile(l + '.pdf', '../' + l + '.pdf')
         print('')
@@ -48,44 +47,22 @@ dirs = [
     '05-MCH/mch-pressure',
 ]
 with cd(dirs[0]):
-    tex_command[-1:] = ['nbuoh-skeletal']
     call(tex_command)
-    shutil.copyfile('nbuoh-skeletal.pdf','../nbuoh-skeletal.pdf')
-
-    tex_command[-1:] = ['sbuoh-skeletal']
-    call(tex_command)
-    shutil.copyfile('sbuoh-skeletal.pdf','../sbuoh-skeletal.pdf')
-
-    tex_command[-1:] = ['tbuoh-skeletal']
-    call(tex_command)
-    shutil.copyfile('tbuoh-skeletal.pdf','../tbuoh-skeletal.pdf')
-
-    tex_command[-1:] = ['ibuoh-skeletal']
-    call(tex_command)
-    shutil.copyfile('ibuoh-skeletal.pdf','../ibuoh-skeletal.pdf')
+    for spec in ['nbuoh', 'sbuoh', 'tbuoh', 'ibuoh']:
+        shutil.copyfile('{}-skeletal.pdf'.format(spec),'../{}-skeletal.pdf'.format(spec))
 
 with cd(dirs[1]):
-    tex_command[-1:] = ['ipeoh-skeletal']
     call(tex_command)
     shutil.copyfile('ipeoh-skeletal.pdf','../ipeoh-skeletal.pdf')
 
 for dir in dirs[2:5]:
     l = dir.split('/')[1]
     with cd(dir):
-        tex_command[-1:] = [l + '-first']
-        call(tex_command)
-        tex_command[-1:] = [l + '-over']
         call(tex_command)
         shutil.copyfile(l + '-first.pdf', '../' + l + '-first.pdf')
         shutil.copyfile(l + '-over.pdf', '../' + l + '-over.pdf')
 
 with cd(dirs[5]):
-    tex_command[-1:] = ['mch-pressure-1']
     call(tex_command)
-    tex_command[-1:] = ['mch-pressure-2']
-    call(tex_command)
-    tex_command[-1:] = ['mch-pressure-3']
-    call(tex_command)
-    shutil.copyfile('mch-pressure-1.pdf', '../mch-pressure-1.pdf')
-    shutil.copyfile('mch-pressure-2.pdf', '../mch-pressure-2.pdf')
-    shutil.copyfile('mch-pressure-3.pdf', '../mch-pressure-3.pdf')
+    for num in ['1', '2', '3']:
+        shutil.copyfile('mch-pressure-{}.pdf'.format(num), '../mch-pressure-{}.pdf'.format(num))
